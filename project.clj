98f5fd6dffe34432825ae4806b9c3bf143f4337f
Roch D'Amour <roch.damour@gmail.com>
2020-04-20 22:38:21 -0400

    Setup json-wrapper and nrepl

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring/ring-json "0.5.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler imageboard-backend.handler/app}
+  :ring {:handler imageboard-backend.handler/app
+         :nrepl {:start? true} }
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- src/imageboard_backend/handler.clj
+++ src/imageboard_backend/handler.clj
@@ -1,11 +1,13 @@
 (ns imageboard-backend.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+(defn handler [request]
+  (response {:foo "bar"}))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-json-response handler))
+
