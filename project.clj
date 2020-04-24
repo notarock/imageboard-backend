8bae50bd678126cd4d0d57f04504acb3f1ec83d9
Roch D'Amour <roch.damour@gmail.com>
2020-04-23 22:43:13 -0400

    Added dotenv & toucan db orm

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-json "0.5.0"]
                  [compojure "1.6.1"]
+                 [toucan "1.15.1"]
+                 [lynxeyes/dotenv "1.0.2"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler imageboard-backend.handler/app
